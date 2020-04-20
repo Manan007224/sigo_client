c34df18ff4255e5a098dbb9dfd1183c7783e74c7
Manan <mmaniyar@splunk.com>
2020-04-19 23:31:48 -0700

    return and close methods for conn_pool

--- pkg/conn_pool.go
+++ pkg/conn_pool.go
@@ -13,15 +13,16 @@ var (
 	closedPool   = errors.New("the pool is closed")
 )
 
+// Most of the ideas and code taken from this blog - http://dustin.sallings.org/2014/04/25/chan-pool.html
 type ConnPool struct {
 	freeConns chan *grpc.ClientConn
 	usedConns chan struct{}
 }
 
-func NewConnPool(poolSize int) *ConnPool {
+func NewConnPool(poolSize int, poolOverflow int) *ConnPool {
 	return &ConnPool{
 		freeConns: make(chan *grpc.ClientConn, poolSize),
-		usedConns: make(chan struct{}, poolSize),
+		usedConns: make(chan struct{}, poolSize+poolOverflow),
 	}
 }
 
@@ -54,8 +55,34 @@ func (cp *ConnPool) Get() (*grpc.ClientConn, error) {
 				return nil, errors.Wrap(err, "failed to create grpc client conn")
 			}
 			return conn, nil
+		case <-timeout.C:
+			return nil, timeOutError
 		}
-	case <-timeout.C:
-		return nil, timeOutError
 	}
+}
+
+func (cp *ConnPool) Return(conn *grpc.ClientConn) {
+	defer func() {
+		if recover() != nil {
+			conn.Close()
+		}
+	}()
+
+	select {
+	case cp.freeConns <- conn:
+	default:
+		<-cp.usedConns
+		conn.Close()
+	}
+}
+
+func (cp *ConnPool) Close() (err error) {
+	defer func() {
+		err, _ = recover().(error)
+	}()
+	close(cp.freeConns)
+	for conn := range cp.freeConns {
+		conn.Close()
+	}
+	return
 }
